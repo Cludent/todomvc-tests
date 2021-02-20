172a5b76cb386a6e357296e0453f37eb950f9a37
Cludent <dimetrij@gmail.com>
2021-02-20 14:08:21 +0200

    after review

--- tests/test_todomvc_E2E.py
+++ tests/test_todomvc_E2E.py
@@ -1,16 +1,18 @@
 from selene.support.conditions import have
 from selene.support.shared import browser
+browser.config.timeout = 7
 
 
 def test_todos_management():
     browser.open('https://todomvc4tasj.herokuapp.com')
-    browser.element('#new-todo').wait_until(have.attribute('[data-woven]'))
+    browser.should(have.js_returned(True, 'return \
+        Object.keys(require.s.contexts._.defined).length === 39'))
 
     # Add
     browser.element('#new-todo').type('a').press_enter()
     browser.element('#new-todo').type('b').press_enter()
     browser.element('#new-todo').type('c').press_enter()
-    browser.all('#todo-list>li').should(have.exact_texts('a','b','c'))
+    browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
 
     # Edit
     browser.all('#todo-list>li').element_by(have.exact_text('a')).double_click()
@@ -23,8 +25,7 @@ def test_todos_management():
 
     # Clear completed
     browser.element('#clear-completed').click()
-    browser.all('#todo-list>li').filtered_by(have.css_class('active'))\
-        .should(have.exact_texts('b','c'))
+    browser.all('#todo-list>li').should(have.exact_texts('b', 'c'))
 
     # Cancel edit
     browser.all('#todo-list>li').element_by(have.exact_text('b')).double_click()
@@ -32,6 +33,6 @@ def test_todos_management():
         .element('.edit').type(' edited').press_escape()
 
     # Delete
-    browser.all('#todo-list>li').element_by(have.exact_text('b'))\
+    browser.all('#todo-list>li').element_by(have.exact_text('b')).hover()\
         .element('.destroy').click()
     browser.all('#todo-list>li').should(have.exact_texts('c'))
