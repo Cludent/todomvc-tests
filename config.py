122c1337c44201e2903aeac810dcb4a3f9972a8d
Cludent <dimetrij@gmail.com>
2021-06-29 03:42:30 +0300

    VIII_with_remote_driver

--- config.py
+++ config.py
@@ -1,20 +1,26 @@
-from typing import Literal
+from typing import Literal, Optional
 import pydantic
 
 EnvContext = Literal['local', 'prod', 'stage']
 EnvBrowserName = Literal['chrome', 'firefox']
 
-class Options(pydantic.BaseSettings):
 
+class Settings(pydantic.BaseSettings):
+    # below are parameters that reads from os env
     context: EnvContext = 'local'
-
     browser_config_timeout: int = 7
     browser_name: EnvBrowserName = 'chrome'
-    browser_headless: bool = False
     browser_quit_after_each_test: bool = False
+    browser_headless: bool = False
+    browser_window_maximize: bool = False
+    browser_window_width: int = 1024
+    browser_window_height: int = 768
+    remote_url: Optional[pydantic.AnyHttpUrl] = None
+    remote_enableVNC: bool = True
+    remote_screenResolution: str = '1024x768x16'
 
 
-options = Options(_env_file=f'config.{Options().context}.env')
+settings = Settings(_env_file=f'config.{Settings().context}.env')
 
 if __name__ == '__main__':
-    print(options.__repr__())
+    print(settings.__repr__())
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,33 +1,58 @@
 import pytest
 from selene.support.shared import browser
-
+from selenium import webdriver
 import config
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    from selenium import webdriver
-    if config.options.browser_name == 'chrome':
-        from webdriver_manager.chrome import ChromeDriverManager
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.headless = config.options.browser_headless
-        chrome_driver = webdriver.Chrome(
-            executable_path=ChromeDriverManager().install(),
-            options=chrome_options)
-        browser.config.driver = chrome_driver
-    elif config.options.browser_name == 'firefox':
-        from webdriver_manager.firefox import GeckoDriverManager
-        firefox_options = webdriver.FirefoxOptions()
-        firefox_options.headless = config.options.browser_headless
-        firefox_driver = webdriver.Firefox(
-            executable_path=GeckoDriverManager().install(),
-            options=firefox_options)
-        browser.config.driver = firefox_driver
-
-    browser.config.timeout = config.options.browser_config_timeout
-    # browser.config.browser_name = config.options.browser_name
+    driver = _set_custom_driver(config.settings)
+    if driver:
+        browser.config.driver = driver
+    else:
+        browser.config.browser_name = config.settings.browser_name
     yield
-    if config.options.browser_quit_after_each_test:
+    if config.settings.browser_quit_after_each_test:
         browser.quit()
     else:
         browser.clear_local_storage()
+
+
+def _set_custom_driver(settings: config.Settings) -> webdriver:
+    options = None
+    if settings.browser_name == 'chrome':
+        options = webdriver.ChromeOptions()
+        options.headless = settings.browser_headless
+    elif settings.browser_name == 'firefox':
+        options = webdriver.FirefoxOptions()
+        options.headless = settings.browser_headless
+
+    if settings.remote_url:
+        options.set_capability('enableVNC', settings.remote_enableVNC)
+        options.set_capability('screen_Resolution', settings.remote_screenResolution)
+        driver = webdriver.Remote(
+            settings.remote_url, options=options
+        )
+    else:
+        from webdriver_manager.chrome import ChromeDriverManager
+        from webdriver_manager.firefox import GeckoDriverManager
+        driver = {
+            'chrome': lambda: webdriver.Chrome(
+                executable_path=ChromeDriverManager().install(),
+                options=options
+            ),
+            'firefox': lambda: webdriver.Firefox(
+                executable_path=GeckoDriverManager().install(),
+                options=options
+            )
+        }[settings.browser_name]()
+
+    if settings.browser_window_maximize:
+        driver.maximize_window()
+    else:
+        driver.set_window_size(
+            width=settings.browser_window_width,
+            height=settings.browser_window_height
+        )
+
+    return driver
